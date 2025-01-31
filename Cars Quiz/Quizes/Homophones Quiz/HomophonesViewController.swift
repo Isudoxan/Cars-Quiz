225144004afc7af9141dbbb2ff836ba5e948fd52
Isudoxan <isudoxan.work@gmail.com>
2025-01-31 13:14:03 +0100

    Homophones viewController has been created.

---
+++ Cars Quiz/Quizes/Homophones Quiz/HomophonesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomophonesViewController.swift
+//  Cars Quiz
+//
+//  Created by Danylo Liubyi on 31.01.2025.
+//
+
+import UIKit
+
+class HomophonesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+        title = "Homophones"
+    }
+}
