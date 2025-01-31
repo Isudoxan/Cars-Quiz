53af54900979ac99a441e2f8eb78d21a31733b34
Isudoxan <isudoxan.work@gmail.com>
2025-01-31 14:21:28 +0100

    Added View and ScrollView to ViewController.

--- Cars Quiz/Quizes/Homophones Quiz/HomophonesViewController.swift
+++ Cars Quiz/Quizes/Homophones Quiz/HomophonesViewController.swift
@@ -9,10 +9,45 @@ import UIKit
 
 class HomophonesViewController: UIViewController {
 
+    // MARK: - UI Components
+    
+        let containerView = UIView()
+        let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .green
+        view.backgroundColor = .white
         title = "Homophones"
+        
+        setupUI()
+    }
+
+    // MARK: - Methods
+
+    private func setupUI() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(containerView)
+
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            containerView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            containerView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            containerView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            containerView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+        ])
     }
 }
