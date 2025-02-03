64d2e8a7197c324980800c378a547a78c3c20ae4
Isudoxan <isudoxan.work@gmail.com>
2025-02-03 18:23:48 +0100

    Homophon's images have been added.

--- Cars Quiz/Quizes/Homophones Quiz/HomophonesImageProvider.swift
+++ Cars Quiz/Quizes/Homophones Quiz/HomophonesImageProvider.swift
@@ -9,6 +9,28 @@ import UIKit
 
 class HomophonesImageProvider {
     
+    class HomophonesImage {
+        let firstWordImage: String
+        let secondWordImage: String
+        
+        init(firstWordImage: String, secondWordImage: String) {
+            self.firstWordImage = firstWordImage
+            self.secondWordImage = secondWordImage
+        }
+    }
+
+    var homophonesImages: [HomophonesImage] = [
+        HomophonesImage(firstWordImage: "WriteImage", secondWordImage: "RightImage"),
+        HomophonesImage(firstWordImage: "RootImage", secondWordImage: "RouteImage"),
+        HomophonesImage(firstWordImage: "WineImage", secondWordImage: "WhineImage"),
+        HomophonesImage(firstWordImage: "WaitImage", secondWordImage: "WeightImage"),
+        HomophonesImage(firstWordImage: "WeakImage", secondWordImage: "WeekImage"),
+        HomophonesImage(firstWordImage: "BawlImage", secondWordImage: "BallImage"),
+        HomophonesImage(firstWordImage: "HoleImage", secondWordImage: "WholeImage"),
+        HomophonesImage(firstWordImage: "HearImage", secondWordImage: "HereImage"),
+        HomophonesImage(firstWordImage: "PreyImage", secondWordImage: "PrayImage"),
+        HomophonesImage(firstWordImage: "ChillyImage", secondWordImage: "ChilliImage")
+        ]
     
     
 }
